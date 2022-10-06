926f3074138664701b40b87a6dfe118a11043b65
Noel Portugal <noel.portugal@workday.com>
2022-10-05 17:50:23 -1000

    Initial Commit

---
+++ FindMyCar/FindMyCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyCarApp.swift
+//  FindMyCar
+//
+//  Created by Noel Portugal on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FindMyCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
